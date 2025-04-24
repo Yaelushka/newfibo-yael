b4233cee2008db5ddaeee07c8dfcfc7f3def80ce
Yaelushka <yael.station@gmail.com>
2025-04-24 15:07:09 +0300

    v2.1

--- fibo.js
+++ fibo.js
@@ -2,29 +2,33 @@ const result = document.querySelector("#result");
 const input =document.getElementById("input");
 const loadingElement = document.querySelector("#loading");
 const resetBtn= document.querySelector(".reset");
-
+const errorElement = document.querySelector("#error");
 
 
 input.addEventListener("change", function() { 
-   displayResult ()   
+  displayResult ()   
 })
-//onblur = (keyUp) =>{};
-
-
-
+ 
 function showLoading(isShow){
   loadingElement.classList.toggle("hidden", !isShow) 
 }
 
+function showError(message){
+   errorElement.textContent = message
+   errorElement.classList.remove("hidden")
+}
+
+function hideError(){  
+   errorElement.textContent = ""
+   errorElement.classList.add("hidden")
+
+}
+
 
 async function searchFibNum(x){ 
-   
-   if(input.value>20 ){
-      console.log("Please enter a number lower than 20")
-   }
+  
    try{
       showLoading(true);
-      
      const Response = await 
      fetch (`http://localhost:3000/calculate/${x}`);
      console.log({Response});
@@ -33,9 +37,11 @@ async function searchFibNum(x){
       throw new Error ("Something went wrong...")
       
    }
+   
    const data = await Response.json();
+   console.log(data.result)
    result.textContent = data.result;
-  
+   
    
    }  catch(err){
       console.log("errrror")   }
@@ -44,13 +50,49 @@ async function searchFibNum(x){
    
 }
 function displayResult (){
-   //const num = parseInt(input.value);
-   searchFibNum(input.value);
+
+   input.style.border = "";
+   input.style.boxShadow = "";
+
+   if(input.value==123){
+      result.textContent = "";
+      showError("418 - I am a teapot");
+      return;
+   }
+
+   if(input.value>20 ){
+      result.textContent = ""
+      showError("Number must be lower than 20")
+      input.style.border = "1px solid red";
+      input.style.boxShadow = "0 0 3px red";
+      return;
+   }
+   if (input.value<=0){
+      result.textContent = "";
+      showError("Number can't be smaller than 1")
+      input.style.border = "1px solid red";
+      input.style.boxShadow = "0 0 3px red";
+      return;
+   }
+  
+   else {
+      hideError();
+      searchFibNum(input.value);
+      
 }
+   }
+  
+;
+
 
 resetBtn.addEventListener("click",function(){
+   input.style.border = "";
+   input.style.boxShadow = "";
    input.value= " "
    result.textContent = ""
+   hideError();
+
+   
 });
 
 
